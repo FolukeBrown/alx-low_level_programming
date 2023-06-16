3f62cf95e5f439ef58e26316031edfd3aa57bf90
FolukeBrown <olorunlosefolke@gmail.com>
2023-06-16 00:39:34 -0700

    0X00.c Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
-*main - Entry point
+*main - A c program that prints a line to the standard 
 *
-*Return: Always 0 (Success)
+*Return 1 (Success)
 */
 int main(void)
 {
      write(2,
-     "all that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+     "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
      return (1);
 }
